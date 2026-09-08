2bf053fb13c629b9ea76abc6bcca175729fcd9b3
aya-lamzoudi <aya.lamzoudi01@gmail.com>
2026-09-08 23:24:18 +0100

    Up date gestion

--- gestion.js
+++ gestion.js
@@ -2,7 +2,7 @@ module.exports = {acheter,annuler }
 var prompt = require("prompt-sync")();
 function acheter(trips, ticket) {
     
-    let found = false;
+    let found = -1;
     let tripId ,price;
     let name = prompt("Veuillez saisir votre nom : ");
     let Id = parseInt(prompt("Veuillez saisir Identifiant du trajet :"));
@@ -12,22 +12,27 @@ function acheter(trips, ticket) {
             seatnumber = parseInt(50 - trips[i].availableSeats);
             tripId = trips[i].id;
             price = trips[i].price;
-            found = true
+            found = 1
             break;
+        } else if (Id == trips[i].id && trips[i].availableSeats < 0) {
+            found=2
         }
     }
-    if (found) {
-        let newticket={
+    if (found == 1) {
+        let newticket = {
             id: ticket.length + 1,
-            passengerName: name, 
-            tripId: tripId, 
-            seatNumber: seatnumber ,
+            passengerName: name,
+            tripId: tripId,
+            seatNumber: seatnumber,
             price: price
         }
         ticket.push(newticket)
         console.log("Votre opération a été un succès.")
         
-    }else console.log("Ce voyage n'est pas disponible.")
+    } else if (found == 2) {
+        console.log("Train complet.")
+        
+    }else  console.log("Ce voyage n'est pas disponible.") 
     
 
 }
